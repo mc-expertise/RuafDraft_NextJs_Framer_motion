030db6c8f8981796e161b44108fa35a401315aab
Mohamed <chouati.dev@gmail.com>
2024-01-09 21:58:02 +0100

    add use client in component

--- app/components/Loader/page.tsx
+++ app/components/Loader/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { useEffect, useState } from 'react';
 import IconLogo from '../Svgs/IconLogo';
 import { slideUp, opacity } from './anim';
--- app/components/anim/GsapRotate.tsx
+++ app/components/anim/GsapRotate.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { ChangeEvent, useEffect, useRef } from 'react';
 import gsap from 'gsap';
 
